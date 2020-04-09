ffeea3809cb3dc30d36ae51a1e58501e532c84e5
Sam <s@sammagee.me>
2020-04-08 23:06:19 -0500

    Fix importing

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix');
 require('laravel-mix-purgecss');
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/index.js', 'public/js/app.js')
    .postCss('resources/css/app.css', 'public/css', [
       require('tailwindcss')
    ]);
